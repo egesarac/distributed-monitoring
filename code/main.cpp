7e41ee5506a98227e3c5bdd2a6c547d5ddfa9588
egesarac <egesarac@gmail.com>
2023-12-21 14:45:47 +0100

    compute initial value expressions

--- code/main.cpp
+++ code/main.cpp
@@ -10,6 +10,60 @@
 //#include <boost>
 using namespace std;
 
+set<string> concatWithDestutter(const set<string> &set1, const set<string> &set2) {
+    set<string> result;
+
+    for (const auto &s1 : set1) {
+        for (const auto &s2 : set2) {
+            if (!s1.empty() && !s2.empty()) {
+                if (s1.rfind(';') == string::npos && s2.find(';') == string::npos) {
+                    if (s1 != s2) {
+                        result.insert(s1 + ";" + s2); 
+                    }
+                    else {
+                        result.insert(s1);
+                    }
+                }
+                else if (s1.rfind(';') == string::npos && s2.find(';') != string::npos) {
+                    if (s1 != s2.substr(0, s2.find(';'))) {
+                        result.insert(s1 + ";" + s2); 
+                    }
+                    else {
+                        result.insert(s2);
+                    }
+                }
+                else if (s1.rfind(';') != string::npos && s2.find(';') == string::npos) {
+                    if (s1.substr(s1.rfind(';') + 1, s1.length()) != s2) {
+                        result.insert(s1 + ";" + s2); 
+                    }
+                    else {
+                        result.insert(s1);
+                    }
+                }
+                else {
+                    if (s1.substr(s1.rfind(';') + 1, s1.length()) != s2.substr(0, s2.find(';'))) {
+                        result.insert(s1 + ";" + s2); 
+                    }
+                    else {
+                        result.insert(s1 + ";" + s2.substr(s2.find(';') + 1, s2.length()));
+                    }
+                }
+            }
+            else {
+                if (!s1.empty()) {
+                    result.insert(s1);
+                }
+                else {
+                    result.insert(s2);
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+
 int main() {
     
     /* get spec */ 
@@ -35,9 +89,9 @@ int main() {
     //uniform_real_distribution<> timeDist(0, d);
     //uniform_real_distribution<> valDist(minVal, maxVal);
 
-    vector<map<int,int>> signals(n);
+    vector<map<int,int>> signalsTemp(n);
 
-    for (auto &sig : signals) {
+    for (auto &sig : signalsTemp) {
         set<int> timeStamps;
         timeStamps.insert(0);
 
@@ -54,60 +108,120 @@ int main() {
 
         int initVal = valDist(gen);
         int ctr = 0;
-        for (auto t : timeStamps) {
+        for (const auto &t : timeStamps) {
             sig[t] = (initVal + ctr) % 2;
             ctr++;
         }
     }
 
+    vector<vector<pair<int,int>>> signals(n);
+
+    for (int i = 0; i < n; i++) {
+        for (const auto &edge : signalsTemp[i]) {
+            signals[i].push_back(make_pair(edge.first, edge.second));
+        }
+    }
+    
+
     /* compute the uncertainty intervals */
     vector<vector<vector<int>>> uncertainties(n);
 
-    int i = 0;
-    for (auto sig : signals) {
-        uncertainties[i].resize(sig.size());
+    for (int i = 0; i < n; i++) {
+        int gi = signals[i].size();
+        uncertainties[i].resize(gi);
 
-        int j = 0;
-        uncertainties[i][j].push_back((-1) * del);
-
-        map<int,int>::iterator it = ++sig.begin();
-        j++;
-        for (; it != sig.end(); it++) {
-            int tj = it->first; 
-            int low = max(uncertainties[i][j-1][0] + del, tj - eps);
+        uncertainties[i][0].push_back(-del);
+        for (int j = 1; j < gi; j++) {
+            int tj = signals[i][j].first;
+            int low = max(uncertainties[i][j - 1][0] + del, tj - eps);
             uncertainties[i][j].push_back(low);
-            j++;
         }
 
-        j = sig.size() - 1;
-        map<int,int>::reverse_iterator rit = sig.rbegin();
-        uncertainties[i][j].push_back(rit->first + eps);
-
-        j--;
-        rit++;
-        for (; next(rit) != sig.rend(); rit++) {
-            int tj = rit->first; 
-            int high = min(uncertainties[i][j+1][1] - del, tj + eps);
+        uncertainties[i][gi - 1].push_back(signals[i][gi - 1].first + eps);
+        for (int j = gi - 2; j > 0; j--) {
+            int tj = signals[i][j].first;
+            int high = min(uncertainties[i][j + 1][1] - del, tj + eps);
             uncertainties[i][j].push_back(high);
-            j--;
         }
-
-        i++;
     }
 
-    /* compute the canonical segmentation */ 
-    set<int> segmentation;
+    /* compute the canonical segmentation */
 
-    for (auto s : uncertainties) { // over signals
-        for (int j = 1; j < s.size(); j++) { // over edges
-            for (auto b : s[j]) { // over uncertainty bounds
-                segmentation.insert(b);
+    set<int> segmentation_temp;
+
+    for (const auto &s : uncertainties) {
+        for (int j = 1; j < s.size(); j++) {
+            for (auto b : s[j]) {
+                segmentation_temp.insert(b);
             }
         }
     }
 
-    /* compute the value expressions (assuming for now the signals are boolean) */
+    vector<int> segmentation;
+    for (const auto &b : segmentation_temp) {
+        segmentation.push_back(b);
+    }
+
+    int numSegments = segmentation.size() - 1;
+
+    /* compute the value expressions TODO: check and improve*/
+    vector<vector<set<string>>> valExprs(n);
     
+    for (int i = 0; i < n; i++) {
+        valExprs[i].resize(numSegments);
+
+        for (int j = 0; j < numSegments; j++) {
+            valExprs[i][j].insert("");
+
+            for (int k = 1; k < uncertainties[i].size(); k++) {
+                if (segmentation[j] == uncertainties[i][k][0] && segmentation[j + 1] == uncertainties[i][k][1]) {
+                    // entire expression
+                    set<string> temp;
+                    string expr = to_string(signals[i][k - 1].second) + ";" + to_string(signals[i][k].second);
+                    temp.insert("");
+                    temp.insert(expr);
+                    valExprs[i][j] = concatWithDestutter(valExprs[i][j], temp);
+                }
+                else if (segmentation[j] == uncertainties[i][k][0] && segmentation[j + 1] < uncertainties[i][k][1]) {
+                    // prefix
+                    set<string> temp;
+                    string expr = to_string(signals[i][k - 1].second) + ";" + to_string(signals[i][k].second);
+                    temp.insert("");
+                    temp.insert(expr);
+                    temp.insert(expr.substr(0, expr.find(';')));
+                    valExprs[i][j] = concatWithDestutter(valExprs[i][j], temp);
+
+                }
+                else if (segmentation[j] > uncertainties[i][k][0] && segmentation[j + 1] == uncertainties[i][k][1]) {
+                    // suffix
+                    set<string> temp;
+                    string expr = to_string(signals[i][k - 1].second) + ";" + to_string(signals[i][k].second);
+                    temp.insert("");
+                    temp.insert(expr);
+                    temp.insert(expr.substr(expr.find(';') + 1, expr.length()));
+                    valExprs[i][j] = concatWithDestutter(valExprs[i][j], temp);
+                }
+                else if (segmentation[j] > uncertainties[i][k][0] && segmentation[j + 1] < uncertainties[i][k][1]) {
+                    // infix
+                    set<string> temp;
+                    string expr = to_string(signals[i][k - 1].second) + ";" + to_string(signals[i][k].second);
+                    temp.insert("");
+                    temp.insert(expr);
+                    temp.insert(expr.substr(0, expr.find(';')));
+                    temp.insert(expr.substr(expr.find(';') + 1, expr.length()));
+                    valExprs[i][j] = concatWithDestutter(valExprs[i][j], temp);
+                }
+            }
+
+            if (valExprs[i][j].size() == 1) {
+                int kk = 0;
+                while (kk < uncertainties[i].size() && uncertainties[i][kk][1] <= segmentation[j]) {
+                    kk++;
+                }
+                valExprs[i][j].insert(to_string(signals[i][kk - 1].second));
+            }
+        }
+    }
 
 
     return 0;
